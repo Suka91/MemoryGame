161edb19630350663f310a2425386850cc08e376
Aleksandar Suka <suka61s@hotmail.com>
2017-02-01 01:09:14 +0100

    Devel: Implemented main menu and score screen

--- MemoryGame.hs
+++ MemoryGame.hs
@@ -6,26 +6,119 @@ import Graphics.UI.SDL as SDL
 import System.Posix.Unistd
 import Control.Exception
 import System.Random
+import qualified Graphics.UI.SDL.TTF as TTF
+import System.Time
+import Data.Time.Clock
 
 initParamsFile = "resources/init_board.txt"
 resourceLocationsFile = "resources/locations.txt"
 background = "resources/background.bmp"
 backTheme = "resources/backTheme.bmp"
+buttonBackground = "resources/buttonBackground.bmp"
 
-main = do 	
-		gen <- getStdGen 
-		let randomNumbers = makeRandomGens 20 gen
-		initParamsCoordsIO <- readFile initParamsFile
-		initParamsLocationsIO <- readFile resourceLocationsFile
-   		let initParamsCoords = map (\(x,y) -> (read x :: Int, read y  :: Int,0,0)) $ parseRawInput (words initParamsCoordsIO)
-   		let initParamsLocations = map (\(x,y) -> y) $ sortBy randomCompare $ zip randomNumbers $ map (\(x,y) -> (read x :: Int, y)) $ parseRawInput (words initParamsLocationsIO)
 
-		let initState = zipWith (\(a,b,c,d) (e,f) -> (a,b,c,d,e,f)) initParamsCoords initParamsLocations
+data MainMenuClick = DUMMY_CLICK | PLAY_GAME_CLICK | SCORE_CLICK	deriving (Enum,Eq)
 
-		
+--font for text
+text :: String
+text = "./resources/text.ttf"
+
+--font for logo
+logo :: String
+logo = "./resources/logo.ttf"
+
+main = do 
+
 		SDL.init [InitEverything]
-		setVideoMode 590 475 32 []
-		startGame initState
+		TTF.init
+		buttonPressedIO <- mainMenuScreen
+		buttonPressed <- buttonPressedIO
+		if (buttonPressed == PLAY_GAME_CLICK)
+			then do
+				a <- gameScreen
+				print a
+				scoreScreen
+			else scoreScreen	
+
+scoreScreen = do
+	renderScoreScreen	
+
+renderScoreScreen = do
+	screen <- getVideoSurface
+	clearScreen <- loadBMP background
+	blitSurface clearScreen Nothing screen Nothing
+	
+	font <- TTF.openFont text 50
+	textSurface <- TTF.renderUTF8Solid font "SCORE" (SDL.Color 255 255 255)
+	blitSurface textSurface Nothing screen (Just(Rect 250 50 0 0))
+
+	SDL.flip screen
+		
+--function for main menu screen
+mainMenuScreen = do
+	setVideoMode 590 475 32 []
+	renderMainMenuScreen
+	return $ loopMainMenu
+	
+loopMainMenu = do
+		event <- waitEvent
+		case event of 
+			MouseButtonDown _ _ _ -> do
+				mainMenuEvent <- handleEventMainMenu event
+				if(mainMenuEvent == DUMMY_CLICK)
+					then loopMainMenu
+					else if(mainMenuEvent == PLAY_GAME_CLICK)
+						then return PLAY_GAME_CLICK
+						else return SCORE_CLICK	
+			_ -> loopMainMenu
+
+handleEventMainMenu (MouseButtonDown mx my ButtonLeft) = do	
+							if((x > 220) && (x < 370) && (y > 270) && (y < 320))
+								then return PLAY_GAME_CLICK
+								else if((x > 220) && (x < 370) && (y > 340) && (y < 390))
+									then return SCORE_CLICK
+									else return DUMMY_CLICK 
+							where x = fromIntegral mx
+					      		      y = fromIntegral my
+								
+renderMainMenuScreen = do
+	screen <- getVideoSurface
+	clearScreen <- loadBMP background
+	blitSurface clearScreen Nothing screen Nothing
+	buttonMainMenu <- loadBMP buttonBackground 
+	blitSurface buttonMainMenu Nothing screen (Just(Rect 220 270 0 0))
+	blitSurface buttonMainMenu Nothing screen (Just(Rect 220 340 0 0))
+	
+	font <- TTF.openFont text 35
+	textSurface <- TTF.renderUTF8Solid font "PLAY" (SDL.Color 0 0 0)
+	blitSurface textSurface Nothing screen (Just(Rect 260 280 0 0))
+
+	font <- TTF.openFont text 35
+	textSurface <- TTF.renderUTF8Solid font "SCORE" (SDL.Color 0 0 0)
+	blitSurface textSurface Nothing screen (Just(Rect 250 350 0 0))
+
+	font <- TTF.openFont logo 130
+	textSurface <- TTF.renderUTF8Solid font "MEMORY GAME" (SDL.Color 255 255 255)
+	blitSurface textSurface Nothing screen (Just(Rect 45 100 0 0))
+
+	SDL.flip screen
+		
+--function for game screen
+gameScreen = do
+
+	beginTime <- getCurrentTime
+	gen <- getStdGen 
+	let randomNumbers = makeRandomGens 20 gen
+	initParamsCoordsIO <- readFile initParamsFile
+	initParamsLocationsIO <- readFile resourceLocationsFile
+	let initParamsCoords = map (\(x,y) -> (read x :: Int, read y  :: Int,0,0)) $ parseRawInput (words initParamsCoordsIO)
+	let initParamsLocations = map (\(x,y) -> y) $ sortBy randomCompare $ zip randomNumbers $ map (\(x,y) -> (read x :: Int, y)) $ parseRawInput (words initParamsLocationsIO)
+
+	--(x,y,flip,pogodjena,id,lokacija)
+	let initState = zipWith (\(a,b,c,d) (e,f) -> (a,b,c,d,e,f)) initParamsCoords initParamsLocations
+
+	startGame initState beginTime
+
 
 makeRandomGens 0 _ = []
 makeRandomGens n gen = let (value, newGen) = random gen :: (Int,StdGen)
@@ -36,9 +129,10 @@ randomCompare (a1,b1) (a2,b2)
      | a2 == a1    = EQ  
      | otherwise = LT
 
-startGame gameState = do
+startGame gameState beginTime = do
 			render gameState
-			loop gameState 0		
+			loop gameState 0 beginTime		
+
 
 handleEvent (MouseButtonDown mx my ButtonLeft) gameState = do
 								render newGameState
@@ -47,17 +141,23 @@ handleEvent (MouseButtonDown mx my ButtonLeft) gameState = do
 							      x = fromIntegral mx
 							      y = fromIntegral my
 
-loop gameState turn = do
-	if(turn == 2) then do
+loop gameState turn beginTime = do
+	if(isGameFinished gameState == True) then do
+		endTime <- getCurrentTime
+		return $ diffUTCTime endTime beginTime
+		
+	else if(turn == 2) then do
 		newGameState <- handleSecondTurn gameState
-		loop newGameState 0
+		loop newGameState 0 beginTime
 	else do
 		event <- waitEvent
 		case event of 
 			MouseButtonDown _ _ _ -> do
 				newGameState <- handleEvent event gameState
-				loop newGameState $ calculateTurn newGameState
-			_ -> loop gameState turn
+				loop newGameState (calculateTurn newGameState) beginTime 
+			_ -> loop gameState turn beginTime
+
+isGameFinished gameState = foldl (\acc (a,b,c,d,e,f) -> if (d == 0) then False else acc) True gameState
 
 handleSecondTurn gameState = return $ map (\(a,b,c,d,e,f) -> if ((c == 1) && (checkForPair e gameState)) then (a,b,c,1,e,f) else (a,b,0,d,e,f)) gameState
 
@@ -68,6 +168,8 @@ checkForPair id gameState = if (2 == foldl (\acc (a,b,c,d,e,f) -> if ((id == e) 
 handleMouseClick x y gameState = map (\(a,b,c,d,e,f) -> if((x > a) && (x < a + 100) && (y > b) && (y < b + 100) && (c == 0) && (d == 0))
 						then (a,b,1,d,e,f)
 						else (a,b,c,d,e,f)) gameState
+
+
 render gameState = do
 			screen <- getVideoSurface
 			clearScreen <- loadBMP background
